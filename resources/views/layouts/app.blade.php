8387e801ee3db5cad14b6273907539c9069e30a9
StefaCar23 <stefanaleksci23@gmail.com>
2018-08-22 19:57:54 +0200

    Login form added to project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,21 @@
               <li role="menuitem"><a href="{{ route('home') }}">Home</a></li>
             <li role="menuitem"><a href="{{ route('clients') }}">Clients</a></li>
             <li role="menuitem"><a href="{{ route('reservations') }}">Reservations</a></li>
+          </ul>
+        </div>
+           <div class="top-bar-right">
+          <ul class="dropdown menu" data-dropdown-menu="tckp8q-dropdown-menu" role="menubar">
+            
+              <li role="menuitem"><a href="{{ route('logout') }}"
+                                     onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit()"
+                                     
+                                     >Logout</a>
+              <form id="logout-form" action="{{route('logout') }}"
+                    method="POST" style="display: none;">{{ csrf_field() }} </form>
+              </li>
+            
+            
             
            
           </ul>
